a1682a4e2588215be8ced11f72588f881205ebf8
Kyle Krick <krkrick@gmail.com>
2020-07-12 21:32:14 -0700

    added graphQLFetch to make calls & display errors

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,34 @@ function jsonDateReviver(key, value) {
     return value;
 }
 
+async function graphQLFetch(query, variables = {}) {
+    try {
+        const response = await fetch('/graphql', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({ query, variables })
+        });
+        const body = await response.text();
+        const result = JSON.parse(body, jsonDateReviver);
+
+        if (result.errors) {
+            const error = result.errors[0];
+
+            if (error.extensions.code == 'BAD_USER_INPUT') {
+                const details = error.extensions.exception.errors.join('\n ');
+
+                alert(`${error.message}:\n ${details}`);
+            } else {
+                alert(`${error.extensions.code}: ${error.message}`);
+            }
+        }
+
+        return result.data;
+    } catch (e) {
+        alert(`Error in sending data to server: ${e.message}`);
+    }
+}
+
 class IssueFilter extends React.Component {
     render() {
         return (
@@ -66,7 +94,7 @@ class IssueAdd extends React.Component {
             owner: form.owner.value,
             title: form.title.value,
             due: new Date(new Date().getTime() + 1000*60*60*24*10) // Add ten days to current date
-        }
+        };
 
         this.props.createIssue(issue);
 
@@ -100,14 +128,11 @@ class IssueList extends React.Component {
                 id
             }
         }`;
-        
-        const response = await fetch('/graphql', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ query, variables: { issue } })
-        });
+        const data = await graphQLFetch(query, { issue });
 
-        this.loadData();
+        if (data) {
+            this.loadData();
+        }
     }
 
     async loadData() {
@@ -117,16 +142,11 @@ class IssueList extends React.Component {
                 created effort due
             }
         }`;
+        const data = await graphQLFetch(query);
 
-        const response = await fetch('/graphql', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json'},
-            body: JSON.stringify({ query })
-        });
-
-        const body = await response.text();
-        const result = JSON.parse(body, jsonDateReviver);
-        this.setState({issues: result.data.issueList});
+        if (data) {
+            this.setState({ issues: data.issueList });
+        }
     }
 
     componentDidMount() {
