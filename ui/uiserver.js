61639a8d4ac6ba114cf5651cda9b9db2266311e3
Kyle Krick <krkrick@gmail.com>
2020-08-30 14:07:14 -0700

    refactored HMR code, commented npm scripts

--- ui/uiserver.js
+++ ui/uiserver.js
@@ -1,12 +1,6 @@
 // Imports
-/* eslint "global-require": "off" */
-/* eslint "import/no-extraneous-dependencies": "off" */
-const webpack = require('webpack');
-const devMiddleware = require('webpack-dev-middleware');
-const hotMiddleware = require('webpack-hot-middleware');
 const express = require('express');
 const proxy = require('http-proxy-middleware');
-const config = require('./webpack.config.js');
 require('dotenv').config();
 
 // Variables
@@ -23,7 +17,13 @@ const enableHMR = (process.env.ENABLE_HMR || 'true') === 'true';
 
 if (enableHMR && (process.env.NODE_ENV !== 'production')) {
   console.log('Adding dev middleware, enabling HMR.');
+  /* eslint "global-require": "off" */
+  /* eslint "import/no-extraneous-dependencies": "off" */
+  const webpack = require('webpack');
+  const devMiddleware = require('webpack-dev-middleware');
+  const hotMiddleware = require('webpack-hot-middleware');
 
+  const config = require('./webpack.config.js');
   config.entry.app.push('webpack-hot-middleware/client');
   config.plugins = config.plugins || [];
   config.plugins.push(new webpack.HotModuleReplacementPlugin());
