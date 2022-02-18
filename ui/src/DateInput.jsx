21f6f8598da03060b9fcea4dcaaf81864b2403eb
Kyle Krick <krkrick@gmail.com>
2022-02-17 21:40:36 -0800

    Implemented DateInput

--- ui/src/DateInput.jsx
+++ ui/src/DateInput.jsx
@@ -1,19 +1,70 @@
 import React from 'react';
 
+function displayFormat(date) {
+  return (date != null) ? date.toDateString() : '';
+}
+
+function editFormat(date) {
+  return (date != null) ? date.toISOString().substr(0, 10) : '';
+}
+
+function unformat(str) {
+  const val = new Date(str);
+  return Number.isNaN(val.getTime()) ? null : val;
+}
+
 export default class DateInput extends React.Component {
   constructor(props) {
     super(props);
     this.state = ({
-      value: this.props,
+      value: editFormat(this.props),
       focused: false,
       valid: true,
     });
+    this.onBlur = this.onBlur.bind(this);
+    this.onFocus = this.onFocus.bind(this);
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onBlur(e) {
+    const { value, valid: oldValue } = this.state;
+    const { onValidityChange, onChange } = this.props;
+    const dateValue = unformat(value);
+    const valid = value === '' || dateValue != null;
+    if (valid !== oldValue && onValidityChange) {
+      onValidityChange(e, valid);
+    }
+    this.setState({ focused: false, valid });
+    if (valid) { onChange(e, dateValue); }
+  }
+
+  onFocus() {
+    this.setState({ focused: true });
+  }
+
+  onChange(e) {
+    if (e.target.value.match(/^[\d-]*$/)) {
+      this.setState({ value: e.target.value });
+    }
   }
 
   render() {
     const { value, valid, focused } = this.state;
+    const { value: origValue, name } = this.props;
+    const className = (!valid && !focused) ? 'invalid' : '';
+    const displayValue = (focused || !valid) ? value : displayFormat(origValue);
     return (
-      <h2>{`This is a stub for date inputs ${value} ${valid} ${focused}`}</h2>
+      <input
+        type="text"
+        size={20}
+        name={name}
+        className={className}
+        value={displayValue}
+        placeholder={focused ? 'YYYY-MM-DD' : null}
+        onFocus={this.onFocus}
+        onBlur={this.onBlur}
+        onChange={this.onChange}
+      />
     );
   }
 }
