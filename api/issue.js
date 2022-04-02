54a1f6db4b81c9289d97252e8ff87aa63d1f93f7
Kyle Krick <krkrick@gmail.com>
2022-04-02 14:07:46 -0700

    added issueDelete API

--- api/issue.js
+++ api/issue.js
@@ -46,6 +46,20 @@ async function update(_, { id, changes }) {
   return savedIssue;
 }
 
+async function remove(_, { id }) {
+  const db = getDb();
+  const issue = await db.collection('issues').findOne({ id });
+  if (!issue) { return false; }
+  issue.deleted = new Date();
+
+  let result = await db.collection('deleted_issues').insertOne(issue);
+  if (result.insertedId) {
+    result = await db.collection('issues').removeOne({ id });
+    return result.deletedCount === 1;
+  }
+  return false;
+}
+
 // Query resolvers
 async function list(_, { status, effortMin, effortMax }) {
   const db = getDb();
@@ -72,5 +86,5 @@ async function get(_, { id }) {
 }
 
 module.exports = {
-  add, update, list, get,
+  add, update, delete: remove, list, get,
 };
--- api/scripts/init.mongo.js
+++ api/scripts/init.mongo.js
@@ -35,6 +35,7 @@ const issues = [
 ];
 
 db.issues.remove({});
+db.deleted_issues.remove({});
 db.issues.insertMany(issues);
 const count = db.issues.count();
 print(`Inserted ${count} issues.`);
@@ -46,3 +47,5 @@ db.issues.createIndex({ id: 1 }, { unique: true });
 db.issues.createIndex({ status: 1 });
 db.issues.createIndex({ owner: 1 });
 db.issues.createIndex({ created: 1 });
+
+db.deleted_issues.createIndex({ id: 1 }, { unique: true });
