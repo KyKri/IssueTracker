105d6f760c06ff88bb4db3683cd3d7f0e98c20a4
Kyle Krick <krkrick@gmail.com>
2022-04-03 16:15:03 -0700

    fixed lint error for imports in IssueTable...

--- ui/src/IssueTable.jsx
+++ ui/src/IssueTable.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link, NavLink, withRouter } from 'react-router-dom';
-import { Button, Glyphicon, Tooltip, OverlayTrigger } from 'react-bootstrap';
+import {
+  Button, Glyphicon, Tooltip, OverlayTrigger,
+} from 'react-bootstrap';
 
 const IssueRow = withRouter(({
   issue,
