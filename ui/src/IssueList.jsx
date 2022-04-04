624e781f87a50c2e9d78ad6ae6c4c0036557ce4b
Kyle Krick <krkrick@gmail.com>
2022-04-03 17:05:47 -0700

    Updated navigation to use react-router-bootstrap

--- ui/src/IssueList.jsx
+++ ui/src/IssueList.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import URLSearchParams from 'url-search-params';
 import { Route } from 'react-router-dom';
-import { Label } from 'react-bootstrap';
 
 import IssueAdd from './IssueAdd.jsx';
 import IssueDetail from './IssueDetail.jsx';
@@ -128,7 +127,6 @@ export default class IssueList extends React.Component {
     const { match } = this.props;
     return (
       <React.Fragment>
-        <h1><Label>Issue Tracker</Label></h1>
         <IssueFilter />
         <hr />
         <IssueTable issues={issues} closeIssue={this.closeIssue} deleteIssue={this.deleteIssue} />
