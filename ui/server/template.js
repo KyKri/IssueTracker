ff86f2c8ba60f10cb60d39de6f7df67cb9ef9236
Kyle Krick <krkrick@gmail.com>
2022-04-15 17:27:22 -0700

    Added template.js to support SSR

---
+++ ui/server/template.js
@@ -0,0 +1,22 @@
+function template(body) {
+  return `
+  <html>
+    <head>
+        <title>Issue Tracker</title>
+        <meta charset="utf-8">
+        <style>
+            table.table-hover tr {cursor: pointer;}
+            .panel-title a {display: block; width: 100%; cursor: pointer;}
+        </style>
+        <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+      <!-- Page generated from template -->
+      <div id="content">${body}</div>
+    </body>
+  </html>
+  `;
+}
+
+module.exports = template;
