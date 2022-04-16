a8617642cef98989e130faa8c3e564d3baac29a3
Kyle Krick <krkrick@gmail.com>
2022-04-16 16:15:26 -0700

    Modified webpack config to support SSR

--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
+const nodeExternals = require('webpack-node-externals');
 
-module.exports = {
+const browserConfig = {
   mode: 'development',
   entry: { app: ['./browser/App.jsx'] },
   output: {
@@ -13,7 +14,19 @@ module.exports = {
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        use: 'babel-loader',
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', {
+                targets: {
+                  ie: '11',
+                },
+              }],
+              '@babel/preset-react',
+            ],
+          },
+        },
       },
     ],
   },
@@ -25,3 +38,43 @@ module.exports = {
   },
   devtool: 'source-map',
 };
+
+const serverConfig = {
+  mode: 'development',
+  entry: { app: ['./server/uiserver.js'] },
+  target: 'node',
+  externals: [nodeExternals()],
+  output: {
+    filename: 'server.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', {
+                targets: { node: 10 },
+              }],
+              '@babel/preset-react',
+            ],
+          },
+        },
+      },
+    ],
+  },
+  optimization: {
+    splitChunks: {
+      name: 'vendor',
+      chunks: 'all',
+    },
+  },
+  devtool: 'source-map',
+};
+
+module.exports = [browserConfig, serverConfig];
