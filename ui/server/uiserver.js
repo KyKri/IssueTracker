d8245713d46603a5bbf41993570f819f62ff76e3
Kyle Krick <krkrick@gmail.com>
2022-04-15 17:35:02 -0700

    Configured uiserver to use SSR for /about

--- ui/server/uiserver.js
+++ ui/server/uiserver.js
@@ -1,14 +1,17 @@
-// Imports
+// Package imports
 const express = require('express');
 const proxy = require('http-proxy-middleware');
 require('dotenv').config();
+const path = require('path');
+
+// Custom imports
+const render = require('./render.js');
 
 // Variables
 const port = process.env.UI_SERVER_PORT || 8000;
 const apiProxyTarget = process.env.API_PROXY_TARGET;
 const UI_API_ENDPOINT = process.env.UI_API_ENDPOINT || 'http://localhost:3000/graphql';
 const env = { UI_API_ENDPOINT };
-const path = require('path');
 
 // Express config
 const app = express();
@@ -43,6 +46,8 @@ if (apiProxyTarget) {
 app.get('/env.js', (req, res) => {
   res.send(`window.ENV = ${JSON.stringify(env)}`);
 });
+
+app.get('/about', render);
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve('public/index.html'));
