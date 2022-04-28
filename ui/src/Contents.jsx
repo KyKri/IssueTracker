25d437af342c75c7cf271c5768846ceb64e5bcfc
Kyle Krick <krkrick@gmail.com>
2022-04-28 16:44:47 -0700

    Refactored routes into its own file

--- ui/src/Contents.jsx
+++ ui/src/Contents.jsx
@@ -1,21 +1,13 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
-import IssueList from './IssueList.jsx';
-import IssueReport from './IssueReport.jsx';
-import IssueEdit from './IssueEdit.jsx';
-import About from './About.jsx';
-import NotFound from './NotFound.jsx';
+import routes from './routes.js';
 
 export default function Contents() {
   return (
     <Switch>
       <Redirect exact from="/" to="/issues" />
-      <Route path="/issues" component={IssueList} />
-      <Route path="/report" component={IssueReport} />
-      <Route path="/edit/:id" component={IssueEdit} />
-      <Route path="/about" component={About} />
-      <Route component={NotFound} />
+      {routes.map(attrs => <Route {...attrs} key={attrs.path} />)}
     </Switch>
   );
 }
