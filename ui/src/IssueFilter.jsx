57ab48e658cdd6f4137b9598089c1e7319a4e7f6
Kyle Krick <krkrick@gmail.com>
2022-05-11 20:17:03 -0700

    Modified IssueFilter to take path from props to make it reusable

--- ui/src/IssueFilter.jsx
+++ ui/src/IssueFilter.jsx
@@ -66,7 +66,7 @@ class IssueFilter extends React.Component {
   }
 
   applyFilter() {
-    const { history } = this.props;
+    const { history, urlBase } = this.props;
     const { status, effortMin, effortMax } = this.state;
 
     const params = new URLSearchParams();
@@ -75,7 +75,7 @@ class IssueFilter extends React.Component {
     if (effortMax) { params.set('effortMax', effortMax); }
 
     const search = params.toString() ? `?${params.toString()}` : '';
-    history.push({ pathname: '/issues', search });
+    history.push({ pathname: urlBase, search });
   }
 
   showOriginalFilter() {
