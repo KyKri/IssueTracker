a9dbb8925c1808cdba00d52f1fec2c79e988e8e9
Kyle Krick <krkrick@gmail.com>
2022-05-15 21:16:26 -0700

    Updated UI to work with pagination

--- ui/src/IssueList.jsx
+++ ui/src/IssueList.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import URLSearchParams from 'url-search-params';
-import { Panel } from 'react-bootstrap';
+import { Panel, Pagination } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
 import IssueDetail from './IssueDetail.jsx';
 import IssueFilter from './IssueFilter.jsx';
@@ -8,6 +9,25 @@ import IssueTable from './IssueTable.jsx';
 import graphQLFetch from './graphQLFetch.js';
 import withToast from './withToast.jsx';
 import store from './store.js';
+
+const SECTION_SIZE = 5;
+
+function PageLink({
+  params, page, activePage, children,
+}) {
+  params.set('page', page);
+  if (page === 0) {
+    return React.cloneElement(children, { disabled: true });
+  }
+  return (
+    <LinkContainer
+      isActive={() => page === activePage}
+      to={{ search: `?${params.toString()}` }}
+    >
+      {children}
+    </LinkContainer>
+  );
+}
 
 class IssueList extends React.Component {
   static async fetchData(match, search, showError) {
@@ -64,12 +84,15 @@ class IssueList extends React.Component {
 
   constructor() {
     super();
-    const issues = store.initialData ? store.initialData.issueList.issues : null;
-    const selectedIssue = store.initialData ? store.initialData.issue : null;
+    const initialData = store.initialData || { issueList: {} };
+    const {
+      issueList: { issues, pages }, issue: selectedIssue,
+    } = initialData;
     delete store.initialData;
     this.state = {
       issues,
       selectedIssue,
+      pages,
     };
     this.closeIssue = this.closeIssue.bind(this);
     this.deleteIssue = this.deleteIssue.bind(this);
@@ -96,7 +119,11 @@ class IssueList extends React.Component {
     const { location: { search }, match, showError } = this.props;
     const data = await IssueList.fetchData(match, search, showError);
     if (data) {
-      this.setState({ issues: data.issueList.issues, selectedIssue: data.issue });
+      this.setState({
+        issues: data.issueList.issues,
+        selectedIssue: data.issue,
+        pages: data.issueList.pages,
+      });
     }
   }
 
@@ -146,10 +173,33 @@ class IssueList extends React.Component {
   }
 
   render() {
-    const {
-      issues, selectedIssue,
-    } = this.state;
+    const { issues } = this.state;
     if (issues == null) { return null; }
+
+    const {
+      selectedIssue, pages,
+    } = this.state;
+    const { location: { search } } = this.props;
+
+    const params = new URLSearchParams(search);
+    let page = parseInt(params.get('page'), 10);
+    if (Number.isNaN(page)) { page = 1; }
+
+    const startPage = Math.floor((page - 1) / SECTION_SIZE) * SECTION_SIZE + 1;
+    const endPage = startPage + SECTION_SIZE - 1;
+    const prevSection = startPage === 1 ? 0 : startPage - SECTION_SIZE;
+    const nextSection = endPage >= pages ? 0 : startPage + SECTION_SIZE;
+
+    const items = [];
+    for (let i = startPage; i <= Math.min(endPage, pages); i += 1) {
+      params.set('page', i);
+      items.push((
+        <PageLink key={i} params={params} activePage={page} page={i}>
+          <Pagination.Item>{i}</Pagination.Item>
+        </PageLink>
+      ));
+    }
+
     return (
       <React.Fragment>
         <Panel>
@@ -162,6 +212,15 @@ class IssueList extends React.Component {
         </Panel>
         <IssueTable issues={issues} closeIssue={this.closeIssue} deleteIssue={this.deleteIssue} />
         <IssueDetail issue={selectedIssue} />
+        <Pagination>
+          <PageLink params={params} page={prevSection}>
+            <Pagination.Item>{'<'}</Pagination.Item>
+          </PageLink>
+          {items}
+          <PageLink params={params} page={nextSection}>
+            <Pagination.Item>{'>'}</Pagination.Item>
+          </PageLink>
+        </Pagination>
       </React.Fragment>
     );
   }
