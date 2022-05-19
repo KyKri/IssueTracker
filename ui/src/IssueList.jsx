5684eeb3e71b0297a0c26f37c37644b5936c7b8b
Kyle Krick <krkrick@gmail.com>
2022-05-18 21:33:21 -0700

    Implemented UI for undoing an issue delete

--- ui/src/IssueList.jsx
+++ ui/src/IssueList.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import URLSearchParams from 'url-search-params';
-import { Panel, Pagination } from 'react-bootstrap';
+import { Panel, Pagination, Button } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
 import IssueDetail from './IssueDetail.jsx';
@@ -96,6 +96,7 @@ class IssueList extends React.Component {
     };
     this.closeIssue = this.closeIssue.bind(this);
     this.deleteIssue = this.deleteIssue.bind(this);
+    this.restoreIssue = this.restoreIssue.bind(this);
   }
 
   componentDidMount() {
@@ -166,8 +167,28 @@ class IssueList extends React.Component {
         newList.splice(index, 1);
         return { issues: newList };
       });
-      showSuccess(`Deleted issue ${id} successfully.`);
+      const undoMessage = (
+        <span>
+          {`Deleted issue ${id} successfully.`}
+          <Button bsStyle="link" onClick={() => this.restoreIssue(id)}>
+            UNDO
+          </Button>
+        </span>
+      );
+      showSuccess(undoMessage);
     } else {
+      this.loadData();
+    }
+  }
+
+  async restoreIssue(id) {
+    const query = `mutation issueRestore($id: Int!) {
+      issueRestore(id: $id)
+    }`;
+    const { showSuccess, showError } = this.props;
+    const data = await graphQLFetch(query, { id }, showError);
+    if (data) {
+      showSuccess(`Issue ${id} restored successfully.`);
       this.loadData();
     }
   }
