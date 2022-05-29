bef4a8da3537e3d1cbf1e7501bdafb17f035cfef
Kyle Krick <krkrick@gmail.com>
2022-05-29 14:21:09 -0700

    Added cookieParser for all routes in API's server.js

--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 // Imports
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const auth = require('./auth.js');
 const { connectToDb } = require('./db');
 const { installHandler } = require('./api_handler.js');
@@ -11,6 +12,7 @@ const port = process.env.API_SERVER_PORT || 3000;
 // Express config
 const app = express();
 
+app.use(cookieParser());
 app.use('/auth', auth.routes);
 
 installHandler(app);
