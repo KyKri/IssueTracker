2d26121f698984b87607aec69b4d962737337ad9
Kyle Krick <krkrick@gmail.com>
2022-06-14 08:00:03 -0700

    Configure Apollo server for CORS

--- api/api_handler.js
+++ api/api_handler.js
@@ -43,7 +43,15 @@ const server = new ApolloServer({
 function installHandler(app) {
   const enableCORS = (process.env.ENABLE_CORS || 'true') === 'true'; // env var is a String, not a boolean
   console.log('CORS setting', enableCORS);
-  server.applyMiddleware({ app, path: '/graphql', cors: enableCORS });
+  let cors;
+  if (enableCORS) {
+    const origin = process.env.UI_SERVER_ORIGIN || 'http://localhost:8000';
+    const methods = 'POST';
+    cors = { origin, methods, credentials: true };
+  } else {
+    cors = 'false';
+  }
+  server.applyMiddleware({ app, path: '/graphql', cors });
 }
 
 module.exports = { installHandler };
